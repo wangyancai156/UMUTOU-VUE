7d2b33bc7dbfe2de3c8f422c3e04bb026993abc5
dupengsim <dupengsim@163.com>
2018-05-16 23:42:59 +0800

    测试提交

--- WangYc.Controllers/WebApi/Account/AccountController.cs
+++ WangYc.Controllers/WebApi/Account/AccountController.cs
@@ -66,7 +66,8 @@ namespace WangYc.Controllers.WebApi.Account
         /// <param name="vcode">验证码</param>
         /// <param name="returnUrl">请求页面</param>
         /// <returns></returns>
-        public string Login(AccountRequest request) {
+        public string Login(AccountRequest request)
+        {
 
             //Windows身份验证,
             //WindowsIdentity userIdentiy = ActiveDirectoryHelper.GetWindowsIdentity(domainName, userName, password);
@@ -74,13 +75,16 @@ namespace WangYc.Controllers.WebApi.Account
             // 基本信息校验
 
             string result = "";
-            if (string.IsNullOrWhiteSpace(request.LoginName)) {
+            if (string.IsNullOrWhiteSpace(request.LoginName))
+            {
                 result = "请输入用户名！";
             }
-            if (string.IsNullOrWhiteSpace(request.PassWord)) {
+            if (string.IsNullOrWhiteSpace(request.PassWord))
+            {
                 result = "密码错误！";
             }
-            if (string.IsNullOrWhiteSpace(request.Vcode)) {
+            if (string.IsNullOrWhiteSpace(request.Vcode))
+            {
                 result = "密码错误！";
             }
             //if (Session[ApplicationSettingsFactory.GetApplicationSettings().VerificationCode] == null || request.Vcode.Trim() != Session[ApplicationSettingsFactory.GetApplicationSettings().VerificationCode].ToString()) {
@@ -88,9 +92,11 @@ namespace WangYc.Controllers.WebApi.Account
             //}
 
             UsersView user = this._usersService.FindUsersBy(request.LoginName);
-            if (user != null) {
+            if (user != null)
+            {
 
-                if (user.UserPwd == request.PassWord) {
+                if (user.UserPwd == request.PassWord)
+                {
 
                     string userData = request.DomainName + "|" + request.LoginName + "|" + request.PassWord;
                     // 添加认证
@@ -99,22 +105,44 @@ namespace WangYc.Controllers.WebApi.Account
                     this._usersService.UpdateLastLoginTime(user.Id);
                     // 重定向回验证前访问的界面
                     //Response.Redirect(FormsAuthentication.GetRedirectUrl(userIdentiy.Name, true));
-    
-                } else {
+
+                }
+                else
+                {
                     result = "密码错误！";
                 }
                 // 清空验证码session，避免资源浪费
                 //Session.Remove(ApplicationSettingsFactory.GetApplicationSettings().VerificationCode);
 
-            } else {
+            }
+            else
+            {
                 result = "用户名错误！";
             }
             return result;
         }
 
-        public UsersView FindUsersBy(string userName) {
+        public UsersView FindUsersBy(string userName)
+        {
 
             return this._usersService.FindUsersBy(userName);
+        }
+
+        /// <summary>
+        ///  TODO：待完善此处的登录校验逻辑，依据账号和密码和数据库中进行比较，然后返回相应实体
+        ///  在vue前台接收返回的实体，进行逻辑判断是否登录成功
+        ///  测试接口：http://localhost:3143/api/account/login?uname=admin&upass=123
+        /// </summary>
+        /// <param name="uname"></param>
+        /// <param name="upass"></param>
+        /// <returns></returns>
+        [System.Web.Http.HttpGet]
+        public UsersView Login(string uname, string upass)
+        {
+            UsersView ent = new UsersView();
+            ent.UserName = "admin";
+            ent.UserPwd = "123";
+            return ent;
         }
 
     }
